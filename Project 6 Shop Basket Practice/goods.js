81ff1b116c933e26c51f7c4b1f9928a47450c919
VolodymyrBorshchuk <Vovkavovka000@gmail.com>
2022-11-15 15:48:23 +0200

    Basket Practice
    Basket Practice

---
+++ Project 6 Shop Basket Practice/goods.js
@@ -0,0 +1,18 @@
+const GOODS = [{
+	id: 1,
+	name: 'Уроки по CSS',
+	price: 1509,
+	imgSrc: './images/html.png',
+},
+{
+	id: 2,
+	name: 'Уроки по CSS',
+	price: 1509,
+	imgSrc: './images/html.png',
+},
+{
+	id: 3,
+	name: 'Уроки по JS',
+	price: 260,
+	imgSrc: './images/html.png',
+}];
